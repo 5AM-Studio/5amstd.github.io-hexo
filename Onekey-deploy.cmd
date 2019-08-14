13484327376a1bd0635eb6269eff344a0022be25
GamerNoTitle <bili33@87ouo.top>
2019-08-14 16:00:44 +0800

    Updated 5 A.M. Website

---
+++ Onekey-deploy.cmd
@@ -0,0 +1,14 @@
+@echo off && title 5 A.M. Studio Website Deployer
+@echo Press any key to start, or close the program
+pause
+REM ask users for starting
+title 5 A.M. Studio Website Deployer - Uploading all the files to github for a backup...
+@echo Uploading all the files to github for a backup...
+git add *.*
+git commit -m "Updated 5 A.M. Website"
+git push
+@echo Press Any Key to Start Deploying to Github
+title 5 A.M. Studio Website Deployer - Deploying to Github Page...
+@echo Deploying to Github Page...
+hexo d -g
+
