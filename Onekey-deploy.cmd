b18a63115cf638f9b35b36e12504cae0f94f5b7c
GamerNoTitle <bili33@87ouo.top>
2019-08-14 16:46:39 +0800

    Updated 5 A.M. Website

--- Onekey-deploy.cmd
+++ Onekey-deploy.cmd
@@ -4,12 +4,12 @@ pause
 REM ask users for starting
 title 5 A.M. Studio Website Deployer - Uploading all the files to github for a backup...
 @echo Uploading all the files to github for a backup...
-git add *.*
-git commit -m "Updated 5 A.M. Website"
 git push
 @echo Press Any Key to Start Deploying to Github
 pause
 title 5 A.M. Studio Website Deployer - Deploying to Github Page...
 @echo Deploying to Github Page...
+git add *.*
+git commit -m "Updated 5 A.M. Website"
 hexo d -g
 
